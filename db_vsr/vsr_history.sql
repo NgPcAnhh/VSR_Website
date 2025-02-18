60c19909056fb6c7395840d0ce6e6fff3c9bb65b
Nguyễn Phúc Anh <katokid7@gmail.com>
2025-02-18 14:57:23 +0700

    Update vsr_history.sql

--- db_vsr/vsr_history.sql
+++ db_vsr/vsr_history.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `history`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `history` (
-  `id_test` bigint DEFAULT NULL,
+  `id_test` varchar(30) DEFAULT NULL,
   `id` int DEFAULT NULL,
   `time` datetime DEFAULT NULL,
   `type` varchar(255) DEFAULT NULL,
